d8fabd7843e2759b0d23375c6708a57ddfd494a0
Sergey <maclovi.dev@gmail.com>
2025-03-17 00:31:06 +0300

    fix: compatibility json logger v=3.3.0

--- src/cats/bootstrap/log.py
+++ src/cats/bootstrap/log.py
@@ -1,12 +1,12 @@
 import logging
 import sys
 
-from pythonjsonlogger import jsonlogger
+from pythonjsonlogger.json import JsonFormatter
 
 
 def setup_logger() -> None:
-    format_ = "%(levelname)s %(asctime)s %(name)s %(funcName)s %(message)s"
-    formatter = jsonlogger.JsonFormatter(format_)
+    fmt = "%(levelname)s %(asctime)s %(name)s %(funcName)s %(message)s"
+    formatter = JsonFormatter(fmt)
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setFormatter(formatter)
     logging.basicConfig(level=logging.INFO, handlers=[stream_handler])
