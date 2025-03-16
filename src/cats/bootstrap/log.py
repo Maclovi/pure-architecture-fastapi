7c3c3b160e3247732177f61a77fd42e9fb1a81a1
Sergey <maclovi.dev@gmail.com>
2025-03-17 00:41:44 +0300

    change json to orjson for logs

--- src/cats/bootstrap/log.py
+++ src/cats/bootstrap/log.py
@@ -1,12 +1,13 @@
 import logging
 import sys
 
+import orjson
 from pythonjsonlogger.json import JsonFormatter
 
 
 def setup_logger() -> None:
     fmt = "%(levelname)s %(asctime)s %(name)s %(funcName)s %(message)s"
-    formatter = JsonFormatter(fmt)
+    formatter = JsonFormatter(fmt, json_serializer=orjson.dumps)
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setFormatter(formatter)
     logging.basicConfig(level=logging.INFO, handlers=[stream_handler])
